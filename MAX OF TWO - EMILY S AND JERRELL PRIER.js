468cf9bfa494551bc5f15af72b4ed58be7e7508f
emilousim <emilousim@gmail.com>
2021-10-12 15:47:44 -0400

    Max of Two working correctly

---
+++ MAX OF TWO - EMILY S AND JERRELL PRIER.js
@@ -0,0 +1,15 @@
+// Emily S. and Jerrell Prier
+
+
+let x = 5;
+let y = 5;
+
+if (x>y){
+    console.log(x);
+}
+else if(x=== y){
+    console.log("The values are the same");
+}
+else{
+    console.log(y);
+}
