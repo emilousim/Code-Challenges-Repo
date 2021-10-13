a546c67473deb17b817e5dcba32be1e3fdb4f450
emilousim <emilousim@gmail.com>
2021-10-13 09:48:47 -0400

    Changed console.log function

--- FIND THE RANGE.js
+++ FIND THE RANGE.js
@@ -1,3 +1,5 @@
+/// Emily S. and Immanuel M. 
+
 var arr = [1, 4, 11, 2, 37, -4];
 
 var max = arr[0];
@@ -12,5 +14,5 @@ for(var i = 0; i < arr.length; i++){
   }
 }
 
-console.log(max);
-console.log(min);
+console.log(min + "min");
+console.log(max + "max");
