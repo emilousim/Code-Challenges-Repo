7b5e99eff158c1377ec611cc63be9e09a1f5731d
emilousim <emilousim@gmail.com>
2021-10-19 12:37:48 -0400

    listed multiple solutions for probblem

--- addToZero - Emily S & Michael B.js
+++ addToZero - Emily S & Michael B.js
@@ -28,3 +28,41 @@ let numbersResult = numbers(arr, 0)
 
 console.log(numbersResult)
 
+
+// --------------------------------------------------------- separating diff ways to solve
+// function takes anumber between 1-10, and returns an array of unique numbers
+
+
+function luckyNumbers(n){
+  let array = [];
+  
+  for (let i = 0; i < n; i++){
+    array.push(Math.floor(Math.random() * 10) +1);
+  }
+  return array;
+}
+console.log(luckyNumbers(4))
+
+// Math.random() will pull a random number between 0-1; * 10 makes it indexes 0-9; + 1 makes its 1-10
+
+
+function luckyNum(n){
+  luckyArr = []
+  let i = 0
+
+  while(i < n){
+
+    let x = Math.floor((Math.random() * 10)+1)
+
+    if(!luckyArr.includes(x)){
+      luckyArr.push(x)
+      i++
+
+    }
+
+  }
+  return luckyArr
+
+}
+
+console.log(luckyNum(4));
