f838bb499d97c5ad7f3b16fe562efe75c519a51c
Emily Simpson <92330317+emilousim@users.noreply.github.com>
2021-11-09 10:54:44 -0500

    Add files via upload

---
+++ outlier code challenge.js
@@ -0,0 +1,14 @@
+//Emily Simpson, Daravy DeRouen, Michael Bodin
+
+let arr = [2, 5, 7, 39, 567, 198, 4, 8]
+const oddNums = arr.filter(n => n%2)
+
+console.log(oddNums)
+
+
+let arr2 = [3, 52, 36, 788, 694, 1, 8]
+const evenNums = arr2.filter(n => n%2 === 0)
+
+console.log(evenNums)
+
+
